195ccd4ad34b1b0aff097472e539251252c39102
temiloluwaseun <precioustutu14@gmail.com>
2024-04-20 09:38:22 +0100

    documenting an alison course

---
+++ alison.py
@@ -0,0 +1,12 @@
+#this is day one learning python again from alison 
+#use single quotesif youhavre double quotes inside your string and vice versa
+print('He said "Well Well Well"') 
+print("It's well")
+#\n is used for new line supposing you want one print statement but you want your words on different lines
+print("line1\nline2")
+#\t is for tab(to add indentations)
+#you caan also use triple quotes for new line
+print("""line1
+line2
+line3""")
+#python executes line by line so if you declare a variable in line 1 and change the value of the variable in line 40 the end answer is th e value in line 40 unless you request an answer before line 40
