efc2b253c45982239c5dd5f6314040ba4347e0ab
temiloluwaseun <precioustutu14@gmail.com>
2024-04-30 22:28:14 +0100

    nothing to see

--- personalisedstory.py
+++ personalisedstory.py
@@ -4,4 +4,4 @@ address=input("where do you live: ")
 struggle=input("what have you struggled with: ")
 help=input("what are you doing to help/get better: ")
 print("My name is "+ name.capitalize() +" . I am "+ age +" years old. I am a resident of " + address.capitalize() +". I struggle with"+ struggle.lower() +
-      ". It is sad but I find my way through it and I've been "+ help.lower()+ " to help myself get better and it's helping. In all, I am still very greatful for life and the love shown by those around me")
+      ". It is sad but I find my way through it and I've been "+ help.lower()+ " to help myself get better and it's helping. In all, I am still very greatful for life and the love shown  by those around me")
