d8f87c7bb85b92de0e1536e68c68ff0329567a1b
temiloluwaseun <precioustutu14@gmail.com>
2024-07-25 22:36:57 +0100

    started a word guess game

--- guesstheword.py
+++ guesstheword.py
@@ -13,7 +13,7 @@ print(display)
 
 # Main game  loop
 while attempts_left > 0:
-    # Opponent guesses aletter
+    # Opponent guesses a letter
     guess = input("Guess a letter: ").lower()
     
     # Validate the guess
